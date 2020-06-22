a3021ccb71f3196831e6780990b0588f7cc3b12a
Siddharth <siddharth.pant@cognizant.com>
2020-06-22 16:17:24 -0600

    Revert "useState" - reverting to class components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,50 +1,45 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 import "./App.css";
 
-const app = (props) => {
-  /*
-  useState works only with functional components
-  useState returns 2 values: the actual state values AND a function to reset the state.
-  Unlike setState in class components, setPersonsState will not update the state of existing keys, it resets the state
-  and can set the state to anything. Section 3, lecture 45
-  */
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
-      { name: "abcd", age: "23", temp: "123" },
+      { name: "abcd", age: "23" , temp:"123"},
       { name: "efgh", age: 25 },
     ],
-    otherState: "some other value",
-  });
+  };
 
-  console.log(personsState) // See the console.log before and after the button is clicked
+  render() {
+    return (
+      <div className="App">
+        <h1>I'm a react app!</h1>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Did you get my information?
+        </Person>
 
-  const switchNameHandler = () => {
-    setPersonsState({
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+      </div>
+    );
+  }
+
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         { name: "ijkl", age: "23" },
         { name: "mnop", age: 25 },
       ],
     });
+    console.log(this.state)
   };
+}
 
-  return (
-    <div className="App">
-      <h1>I'm a react app!</h1>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      >
-        Did you get my information?
-      </Person>
 
-      <button onClick={switchNameHandler}>Switch Name</button>
-    </div>
-  );
-};
-
-export default app;
+export default App;
