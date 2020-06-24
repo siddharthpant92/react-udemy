fc33d74562632fc754e8755321660e4ada5157a1
Siddharth <siddharth.pant@cognizant.com>
2020-06-24 12:33:38 -0600

    radium - :hover

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Person from "./Person/Person";
+import Radium from 'radium'
 import "./App.css";
 
 class App extends Component {
@@ -18,6 +19,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
     let persons = null;
 
@@ -40,6 +46,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "blue",
+        color: "black"
+      }
     }
 
     let classes = [];
@@ -87,4 +97,4 @@ class App extends Component {
   };
 }
 
-export default App;
+export default Radium(App);
