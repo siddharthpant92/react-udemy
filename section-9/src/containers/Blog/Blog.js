ac2fb9dc179af4f01b1a642c2c94e36ec88cb762
Siddharth <siddharth.pant@cognizant.com>
2020-06-29 22:49:26 -0600

    fetching data using axios, displaying data

--- section-9/src/containers/Blog/Blog.js
+++ section-9/src/containers/Blog/Blog.js
@@ -6,20 +6,44 @@ import "./Blog.css";
 import axios from "axios";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+  };
+
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts");
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Sid",
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    });
   }
 
+  postClicked = (postId) => {
+    this.setState({ selectedPostId: postId });
+  };
+
   render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={this.postClicked.bind(this, post.id)}
+        />
+      );
+    });
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost selectedPostId={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
