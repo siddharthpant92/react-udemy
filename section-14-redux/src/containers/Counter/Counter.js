4564a5f0ed518762ce594546cb5fb0ce397555bd
Siddharth <siddharth.pant@cognizant.com>
2020-07-03 17:48:03 -0600

    setup redux for react app, reducer, store, dispatcher, assignment 4

--- section-14-redux/src/containers/Counter/Counter.js
+++ section-14-redux/src/containers/Counter/Counter.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCouner}
+        />
+        <CounterControl label="Add 5" clicked={() => this.props.onAdd(5)} />
+        <CounterControl
+          label="Subtract 5"
+          clicked={() => this.props.onSubtract(5)}
+        />
+      </div>
+    );
+  }
 }
 
-export default Counter;
+// Configuration to only get the state that this container needs
+const mapStateToProps = (state) => {
+  // 'state' refers to what is managed by redux in reducer.js, returns a property called 'ctr'
+  return {
+    ctr: state.counter,
+  };
+};
+
+// Configuration for the actions that this container needs to be dispatch
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCouner: () => dispatch({ type: "DECREMENT" }),
+    onAdd: (addValue) => dispatch({ type: "ADD", value: addValue }),
+    onSubtract: (subValue) => dispatch({ type: "SUBTRACT", value: subValue }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
