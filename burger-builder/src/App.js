5953238d4f24f563f22ea4c4c7f1fc2cb975824b
Siddharth <siddharth.pant@cognizant.com>
2020-07-13 11:00:30 -0600

    converted App.js to functional component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -18,35 +18,34 @@ const asyncAuth = asyncComponent(() => {
   return import("./containers/Auth/Auth");
 });
 
-class App extends Component {
-  render() {
-    let routes = (
+const App = (props) => {
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={asyncAuth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
+        <Route path="/checkout" component={asyncCheckout} />
+        <Route path="/orders" component={asyncOrders} />
+        <Route path="/logout" component={Logout} />
         <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/checkout" component={asyncCheckout} />
-          <Route path="/orders" component={asyncOrders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={asyncAuth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-    return (
-      <BrowserRouter>
-        <Layout>{routes}</Layout>
-      </BrowserRouter>
-    );
   }
-}
+
+  return (
+    <BrowserRouter>
+      <Layout>{routes}</Layout>
+    </BrowserRouter>
+  );
+};
 
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.token !== null,
