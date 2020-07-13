aff02520ad0b0c1707d1e4d2dbea370bb13ee996
Siddharth <siddharth.pant@cognizant.com>
2020-07-13 12:39:38 -0600

    shouldComponentUpdate in functional component using React.memo

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -2,55 +2,6 @@ import React from "react";
 import BurgerIngredientStyle from "./BurgerIngredient.module.css";
 import PropTypes from "prop-types";
 
-// class BurgerIngredient extends Component {
-//   render() {
-//     let ingredient = null;
-
-//     switch (this.props.type) {
-//       case "bread-bottom":
-//         ingredient = <div className={BurgerIngredientStyle.BreadBottom}></div>;
-//         break;
-
-//       case "bread-top":
-//         ingredient = (
-//           <div className={BurgerIngredientStyle.BreadTop}>
-//             <div className={BurgerIngredientStyle.Seeds1}></div>
-//             <div className={BurgerIngredientStyle.Seeds2}></div>
-//           </div>
-//         );
-//         break;
-
-//       case "meat":
-//         ingredient = <div className={BurgerIngredientStyle.Meat}></div>;
-//         break;
-
-//       case "cheese":
-//         ingredient = <div className={BurgerIngredientStyle.Cheese}></div>;
-//         break;
-
-//       case "salad":
-//         ingredient = <div className={BurgerIngredientStyle.Salad}></div>;
-//         break;
-
-//       case "bacon":
-//         ingredient = <div className={BurgerIngredientStyle.Bacon}></div>;
-//         break;
-
-//       default:
-//         ingredient = null;
-//         break;
-//     }
-
-//     return ingredient;
-//   }
-// }
-
-// BurgerIngredient.propTypes = {
-//   type: PropTypes.string.isRequired
-// }
-
-// export default BurgerIngredient;
-
 const burgerIngredient = (props) => {
   let ingredient = null;
 
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,32 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import ModalStyle from "./Modal.module.css";
 import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    // Comparing children, since the component should re-render for OrderSummary and Spinner child components
-    return nextProps.show !== this.props.show || this.props.children !== nextProps.children;
-  }
+const Modal = (props) => {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // Comparing children, since the component should re-render for OrderSummary and Spinner child components
+  //   return nextProps.show !== props.show || props.children !== nextProps.children;
+  // }
 
-  render() {
-    return (
-      <Aux>
-        <Backdrop
-          show={this.props.show}
-          backdropClicked={this.props.modalClosed}
-        />
-        <div
-          className={ModalStyle.Modal}
-          style={{
-            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
-            opacity: this.props.show ? "1" : "0",
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
-}
-export default Modal;
+  return (
+    <Aux>
+      <Backdrop show={props.show} backdropClicked={props.modalClosed} />
+      <div
+        className={ModalStyle.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
+export default React.memo(Modal, (prevProps, nextProps) => {
+  // Opposite logic of componentShould
+  return (
+    nextProps.show === prevProps.show ||
+    prevProps.children === nextProps.children
+  );
+});
