26893bc378493d8bfb08f217593d5b83555cc068
Siddharth <siddharth.pant@cognizant.com>
2020-07-13 11:49:52 -0600

    converted <Orders> and <Checkout> to functional components

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,38 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Order from "../../components/Order/Order";
 import * as actionTypes from "../../store/actions/indexActions";
 import { connect } from "react-redux";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import ErrorModal from "../../components/UI/ErrorModal/ErrorModal";
 
-class Orders extends Component {
-  componentWillMount() {
-    this.props.onFetchOrders();
-  }
+const Orders = (props) => {
+  // Mimicing componentDidMount
+  useEffect(() => {
+    props.onFetchOrders();
+  }, []);
 
-  render() {
-    let orders = <Spinner />;
+  let orders = <Spinner />;
 
-    if (!this.props.order.loading) {
-      if (this.props.order.orders.length > 0) {
-        orders = this.props.order.orders.map((order) => {
-          return (
-            <Order
-              key={order.id}
-              totalPrice={order.price}
-              ingredients={order.ingredients}
-            />
-          );
-        });
-      } else if (this.props.order.firebaseError) {
-        orders = <ErrorModal show={true} />;
-      } else {
-        orders = <p>No existing orders</p>;
-      }
+  if (!props.order.loading) {
+    if (props.order.orders.length > 0) {
+      orders = props.order.orders.map((order) => {
+        return (
+          <Order
+            key={order.id}
+            totalPrice={order.price}
+            ingredients={order.ingredients}
+          />
+        );
+      });
+    } else if (props.order.firebaseError) {
+      orders = <ErrorModal show={true} />;
+    } else {
+      orders = <p>No existing orders</p>;
     }
-    return <div>{orders}</div>;
   }
-}
+
+  return <div>{orders}</div>;
+};
 
 const mapStateToProps = (state) => {
   return {
